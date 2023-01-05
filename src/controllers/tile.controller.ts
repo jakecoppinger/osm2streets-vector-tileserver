1650462573107493d36e292ea69c9a6baf61eda7
Jake Coppinger <jake@jakecoppinger.com>
2023-01-05 12:22:55 +1100

    Generating separate tile layers appears to work

--- src/controllers/tile.controller.ts
+++ src/controllers/tile.controller.ts
@@ -58,7 +58,8 @@ function validateNumberParam({ param, paramName, ctx }: { param: string, paramNa
   return true;
 }
 
-async function fetchOverpassXMLAndValidate({ zoom, x, y, ctx }: { zoom: number, x: number, y: number, ctx: RouterContext }) {
+async function fetchOverpassXMLAndValidate({ zoom, x, y, ctx }:
+  { zoom: number, x: number, y: number, ctx: RouterContext }): Promise<string | null> {
   const url = generateOverpassTurboQueryUrl({ zoom, x, y });
   // HIT http://localhost:3000/tile/16/60293/39332 TO TEST :)
   try {
@@ -79,6 +80,27 @@ async function fetchOverpassXMLAndValidate({ zoom, x, y, ctx }: { zoom: number, 
     console.error("ERROR: Failed to make request to overpass. Is the Docker container running?");
     return null;
   }
+}
+
+function generateTileIndex(geojson: any, ctx: RouterContext) {
+  const tileIndex = geojsonvt(geojson, {
+    maxZoom: 24,  // max zoom to preserve detail on; can't be higher than 24
+    tolerance: 3, // simplification tolerance (higher means simpler)
+    extent: 4096, // tile extent (both width and height)
+    buffer: 64,   // tile buffer on each side
+    debug: 0,     // logging level (0 to disable, 1 or 2)
+    lineMetrics: false, // whether to enable line metrics tracking for LineString/MultiLineString features
+    promoteId: null,    // name of a feature property to promote to feature.id. Cannot be used with `generateId`
+    generateId: false,  // whether to generate feature ids. Cannot be used with `promoteId`
+    indexMaxZoom: 5,       // max zoom in the initial tile index
+    indexMaxPoints: 100000 // max number of points per tile in the index
+  });
+  if (tileIndex === null) {
+    ctx.status = 500;
+    ctx.body = "Error: unable to generate vector tile from geojson";
+    return null;
+  }
+  return tileIndex
 }
 
 export default class UserController {
@@ -121,46 +143,52 @@ export default class UserController {
       osm2lanes: false,
     });
     console.log("Generating geojson (currently you need to choose which features in code)...");
-    const geometry = network.toGeojsonPlain();
-    const lanePolygons = network.toLanePolygonsGeojson();
-    const laneMarkings = network.toLaneMarkingsGeojson();
+    const geometryFeatures = network.toGeojsonPlain();
+    const lanePolygonFeatures = network.toLanePolygonsGeojson();
+    const laneMarkingFeatures = network.toLaneMarkingsGeojson();
 
     // TODO: This needs improving, I don't know much about GeoJSON but it works!
-    const geojson: any = {
+    // const geojson: any = {
+    //   type: "FeatureCollection",
+    //   features: [...JSON.parse(lanePolygonsFeatures).features, ...JSON.parse(geometryFeatures).features, ...JSON.parse(laneMarkingsFeatures).features]
+    // };
+    const geometryGeoJSON = {
       type: "FeatureCollection",
-      features: [...JSON.parse(lanePolygons).features, ...JSON.parse(geometry).features, ...JSON.parse(laneMarkings).features]
+      features: [...JSON.parse(geometryFeatures).features]
+    };
+    const lanePolygonGeoJSON = {
+      type: "FeatureCollection",
+      features: [...JSON.parse(lanePolygonFeatures).features]
+    };
+
+    const laneMarkingGeoJSON = {
+      type: "FeatureCollection",
+      features: [...JSON.parse(laneMarkingFeatures).features]
     };
 
     console.log("Generating tileindex...");
-    const tileIndex = geojsonvt(geojson, {
-      maxZoom: 24,  // max zoom to preserve detail on; can't be higher than 24
-      tolerance: 3, // simplification tolerance (higher means simpler)
-      extent: 4096, // tile extent (both width and height)
-      buffer: 64,   // tile buffer on each side
-      debug: 0,     // logging level (0 to disable, 1 or 2)
-      lineMetrics: false, // whether to enable line metrics tracking for LineString/MultiLineString features
-      promoteId: null,    // name of a feature property to promote to feature.id. Cannot be used with `generateId`
-      generateId: false,  // whether to generate feature ids. Cannot be used with `promoteId`
-      indexMaxZoom: 5,       // max zoom in the initial tile index
-      indexMaxPoints: 100000 // max number of points per tile in the index
-    });
-    if (tileIndex === null) {
-      ctx.status = 500;
-      ctx.body = "Error: unable to generate vector tile from geojson";
+
+    const geometryTileIndex = await generateTileIndex(geometryGeoJSON, ctx);
+    const lanePolygonTileIndex = await generateTileIndex(lanePolygonGeoJSON, ctx);
+    const laneMarkingTileIndex = await generateTileIndex(laneMarkingGeoJSON, ctx);
+
+    if (geometryTileIndex === null || lanePolygonTileIndex === null || laneMarkingTileIndex === null) {
       return;
     }
 
-
     console.log("Generating tile...");
-    const tile = tileIndex.getTile(zoom, x, y);
-    if (tile === null) {
+    const geometryTile = geometryTileIndex.getTile(zoom, x, y);
+    const lanePolygonTile = lanePolygonTileIndex.getTile(zoom, x, y);
+    const laneMarkingTile = laneMarkingTileIndex.getTile(zoom, x, y);
+
+    if (geometryTile === null || lanePolygonTile === null || laneMarkingTile === null) {
       ctx.status = 500;
-      ctx.body = "Error: Coudn't get tile from geojsonvt";
+      ctx.body = "Error: Coudn't get one of the tiles from geojsonvt";
       return;
     }
 
     // This is a Uint8Array
-    const rawArray = vtpbf.fromGeojsonVt({ 'geojsonLayer': tile });
+    const rawArray = vtpbf.fromGeojsonVt({ geometry: geometryTile, lanePolygons: lanePolygonTile, });
     const buf = Buffer.from(rawArray);
 
     sendProtobuf(ctx, buf);
