4b5ae588cd04b7fe06d3c4fe4809ad73e8b892dc
Jake Coppinger <jake@jakecoppinger.com>
2023-01-15 14:53:24 +1100

    Fixed jest import issues

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,8 +1,9 @@
+import { TileCoordinate } from '../src/interfaces';
 
-describe('hello', () => {
-  describe('add', () => {
-    it('should be passing', () => {
-      expect(1).toBe(1);
-    });
+describe('calculateTileCoordsForZoom', () => {
+  it('Should zoom out one level correctly', () => {
+    // const input: TileCoordinate = {zoom :17, x:120587, y: 78648};
+    // const output: TileCoordinate = {zoom :17, x:120587, y: 78648};
+    expect(1).toBe(1);
   });
 });
